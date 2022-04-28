9cd0653933d1cae26a93f8e1beeca6814e0f0339
btruj007 <102835844+btruj007@users.noreply.github.com>
2022-04-28 09:32:10 -0700

    Update Heap.cpp

--- Heap.cpp
+++ Heap.cpp
@@ -1,46 +1,44 @@
-
-#include "Heap.h"
 #include <iostream>
+#include "Heap.h"
 
 using namespace std;
-
 
 Heap::Heap() {
     numItems = 0;
 }
 
-void Heap::enqueue(PrintJob *item) {
-//When queuing multiple PrintJobs, program prints only the job at priority 1
-    if(numItems < MAX_HEAP_SIZE) {
+void Heap::enqueue(PrintJob* item) {
+    //When queuing multiple PrintJobs, program prints only the job at priority 1
+    if (numItems < MAX_HEAP_SIZE) {
         arr[numItems] = item;
-        trickleDown(numItems);
+        trickleUp(numItems);
         numItems++;
     }
 }
 
 void Heap::dequeue() {
-    if(numItems == 1) {
+    if (numItems == 1) {
         arr[0] = 0;
         numItems = 0;
         return;
     }
-    if(numItems > 1) {
-        arr[0] = arr[numItems-1];
-        arr[numItems-1] = 0;
+    if (numItems > 1) {
+        arr[0] = arr[numItems - 1];
+        arr[numItems - 1] = 0;
         numItems--;
         trickleDown(0);
     }
 }
 
 PrintJob* Heap::highest() {
-    if(numItems == 0) {
+    if (numItems == 0) {
         return 0;
     }
     return arr[0];
 }
 
 void Heap::print() {
-    if(numItems == 0) {
+    if (numItems == 0) {
         return;
     }
     else {
@@ -51,13 +49,25 @@ void Heap::print() {
     return;
 }
 
-void Heap::trickleDown(int val) {
-//    for(unsigned i = 0; i < numItems; i++) {
-//        if(arr[val]->getPriority() < arr[i]->getPriority())
-//        {
-//            PrintJob* temp = arr[val];
-//            arr[val] = arr[i];
-//            arr[i] = temp;
+void Heap::trickleDown(int val)
+{
+    for (int i = 0; i < numItems; i++) {
+        if (arr[val]->getPriority() < arr[i]->getPriority())
+        {
+            PrintJob* temp = arr[val];
+            arr[val] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
+void Heap::trickleUp(int val)
+{
+    for (int i = 0; i < numItems; i++) {
+        if (arr[val]->getPriority() > arr[i]->getPriority())
+        {
+            PrintJob* temp = arr[val];
+            arr[val] = arr[i];
+            arr[i] = temp;
         }
     }
 }
