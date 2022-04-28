d82d4715a8cbeb75960dd0a785c025c60031ffec
btruj007 <102835844+btruj007@users.noreply.github.com>
2022-04-28 08:27:54 -0700

    Update PrintJob.cpp

--- PrintJob.cpp
+++ PrintJob.cpp
@@ -1,1 +1,15 @@
+//given
+#include "PrintJob.h"
 
+PrintJob::PrintJob ( int setP, int setJ, int numP ):priority(setP), jobNumber(setJ), numPages(numP){}
+int PrintJob::getPriority ( ){
+    return priority;
+}
+
+int PrintJob::getJobNumber ( ){
+    return jobNumber;
+}
+
+int PrintJob::getPages ( ){
+    return numPages;
+}
